b94b8839876f75e11d549ab6e41b9e3df812752e
DarjaF <carrot.the.oblivion@gmail.com>
2025-08-26 19:58:50 +0300

    first commit

---
+++ src/main/java/Radio.java
@@ -0,0 +1,58 @@
+package ru.netology.javaqa;
+
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int stationNumber) {
+        if (stationNumber >= 0 && stationNumber <= 9) {
+            currentStation = stationNumber;
+        } else {
+            System.out.println("Неверный номер станции.");
+        }
+    }
+
+    public void nextStation() {
+        if (currentStation == 9) {
+            currentStation = 0;
+        } else {
+            currentStation++;
+        }
+    }
+
+    public void previousStation() {
+        if (currentStation == 0) {
+            currentStation = 9;
+        } else {
+            currentStation--;
+        }
+    }
+
+    public int getVolume() {
+        return currentVolume;
+    }
+
+    public void setVolume(int newcurrentVolume) {
+        if (newcurrentVolume >= 0 && newcurrentVolume <= 100) {
+            currentVolume = newcurrentVolume;
+        } else {
+            System.out.println("Уровень громкости вне допустимого диапазона.");
+        }
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 100) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        }
+    }
+}
